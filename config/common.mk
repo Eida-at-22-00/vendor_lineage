89581599e523763e4daae8dc5fe88ebf304e8690
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-05 09:59:52 +0800

    config: Add flag for updater
    Change-Id: Ia34b5ef9bd0be37cb21838e9731a0f132dd5d046

--- config/common.mk
+++ config/common.mk
@@ -149,11 +149,16 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_PACKAGES += \
-    LineageSettingsProvider \
+    LineageSettingsProvider
+
+TARGET_OTA_ENABLED ?= true
+ifeq ($(TARGET_OTA_ENABLED),true)
+PRODUCT_PACKAGES += \
     Updater
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
+endif
 
 # Config
 PRODUCT_PACKAGES += \
