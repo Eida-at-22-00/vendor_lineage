8c095d6ce853921b90a3859a1ab72a689126d5ef
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-18 08:15:15 +0800

    version: remove beta tag
    Change-Id: I77e5be5bd96bd1736c4f6ea56a9a64f1d12db011

--- config/version.mk
+++ config/version.mk
@@ -1,6 +1,6 @@
 PRODUCT_VERSION_MAJOR = 1
 PRODUCT_VERSION_MINOR = 3
-PRODUCT_RELEASE_TYPE = BETA
+PRODUCT_RELEASE_TYPE = NIGHTLY
 
 CURRENT_DEVICE := $(wordlist 2,3,$(subst _, ,$(TARGET_PRODUCT)))
 DEVICE_LIST := $(file < vendor/official_devices/OTA/axion.devices)
